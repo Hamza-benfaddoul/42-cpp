62c4e2fd8da0005014c5834fa1f2b914228564d8
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-06-05 14:04:27 +0100

    Day01 ex01 is done

---
+++ cpp01/ex01/zombieHorde.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 13:43:49 by hbenfadd          #+#    #+#             */
+/*   Updated: 2023/06/05 13:59:14 by hbenfadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name )
+{
+    Zombie *zombie = new Zombie[N];
+    for (int i = 0; i < N; i++)
+        zombie[i].setName(name);
+    return (zombie);
+}
