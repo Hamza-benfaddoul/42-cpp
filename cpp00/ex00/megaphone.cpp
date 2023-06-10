95a32c64d80b8af9103ca6d97737e204d62abb9a
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-06-10 11:39:09 +0100

    update cpp00/ex00/megaphone.cpp

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 12:36:18 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/06/05 12:36:19 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/06/10 11:36:26 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,21 @@ int	main(int ac, char **av)
 {
 	int	i;
 	int	j;
+	std::string str;
 
-	i = 1;
-	while(i < ac)
+	i = 0;
+	if (ac == 1)
 	{
-		j = 0;
-		while(av[i][j])
-		{
-			if (av[i][j] >= 'a' && av[i][j] <= 'z')
-				std::cout << (char)(av[i][j] - 32);
-			else
-				std::cout << av[i][j];
-			j++;
-		}
-		i++;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout<<std::endl;
+		return (EXIT_SUCCESS);
+	}
+	while(++i < ac)
+	{
+		str = av[i];
+		j = -1;
+		while(str[++j])
+			std::cout << (char)toupper(str[j]);
 	}
 	std::cout<<std::endl;
 	return (EXIT_SUCCESS);
