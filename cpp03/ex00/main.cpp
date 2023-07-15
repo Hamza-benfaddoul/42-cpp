7c3cec7f66bff6a467c026e029f56c6b6fb637cb
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-07-15 17:10:05 +0100

    add heder

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -1,11 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 17:07:01 by hbenfadd          #+#    #+#             */
+/*   Updated: 2023/07/15 17:08:42 by hbenfadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "ClapTrap.hpp"
 
-int	main(void){
-	ClapTrap foo("foo");
+int	main(void)
+{
+	ClapTrap clap("Clap");
+	ClapTrap clap2("Clap2");
 
-	foo.attack("boo");
-
-	return (0);
+	clap.attack("Clap2");
+	clap2.takeDamage(10);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
+	clap.attack("Clap2");
+	clap2.takeDamage(1);
+	clap2.attack("Clap");
+	clap.takeDamage(1);
 }
-
