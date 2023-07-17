db038a10c84dd17c6b8aff064e6bf6ddba9ea1ef
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-07-17 07:32:30 +0100

    done

---
+++ cpp03/ex01/ClapTrap.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 17:06:58 by hbenfadd          #+#    #+#             */
+/*   Updated: 2023/07/16 10:52:35 by hbenfadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+ClapTrap::ClapTrap():_name("Unknown"), _hit_points(100), _energy_points(50), _attack_damage(20){}
+
+ClapTrap::ClapTrap(std::string name): _name(name), _hit_points(100), _energy_points(50), _attack_damage(20) {}
+
+ClapTrap::ClapTrap(const ClapTrap& other):
+	_name(other._name),
+	_hit_points(other._hit_points),
+	_energy_points(other._energy_points),
+	_attack_damage(other._attack_damage)
+{}
+
+ClapTrap::~ClapTrap(){}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other){
+	_name = other._name;
+	_hit_points = other._hit_points;
+	_attack_damage = other._attack_damage;
+	return (*this);
+}
+
+void ClapTrap::attack(const std::string& target){
+	if (_energy_points && _hit_points)
+	{
+		std::cout << "ClapTrap " << _name << " attacks " << target << " causing " << _attack_damage << " points of damage!" <<std::endl;
+		_energy_points--;
+	}
+	else
+		std::cout << _name <<  " can't attacks " << target << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount){
+
+	if (_energy_points && _hit_points)
+	{
+		if (_hit_points - amount)
+			_hit_points-= amount;
+		else
+			_hit_points = 0;
+
+		std::cout << "ClapTrap " << _name << " take " << amount << " damages." <<std::endl;
+	}
+	else
+		std::cout << _name <<  " is dead" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount){
+	if (_energy_points && _hit_points)
+	{
+		std::cout << "ClapTrap " << _name << " is repaired of " << amount << " points." <<std::endl;
+		if (_hit_points + amount < 10)
+			_hit_points += amount;
+		else
+			_hit_points = 10;
+		_energy_points--;
+	}
+	else
+		std::cout << _name <<  " is dead" << std::endl;
+}
+
---
+++ cpp03/ex01/ScavTrap.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/15 17:28:24 by hbenfadd          #+#    #+#             */
+/*   Updated: 2023/07/16 10:58:29 by hbenfadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_H
+# define SCAVTRAP_H
+# include <iostream>
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap
+{
+public:
+    ScavTrap();
+    ScavTrap(std:string name);
+    ~ScavTrap();
+
+
+};
+
+#endif /* SCAVTRAP_H */
