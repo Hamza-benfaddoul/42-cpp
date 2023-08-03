276d18e8be82dd6e4038e81a77257897833866a8
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-08-03 16:19:17 +0100

    Update ex00

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,19 +6,22 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:26:17 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/07/27 11:27:08 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/08/03 16:08:58 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-int main(void)
+int main()
 {
-  Cat cat;
-
-  cat.makeSound();
-  Animal *animal = new Dog();
-  animal->makeSound();
-  
+  const Animal *meta = new Animal();
+  const Animal *j = new Dog();
+  const Animal *i = new Cat();
+  std::cout << j->getType() << " " << std::endl;
+  std::cout << i->getType() << " " << std::endl;
+  i->makeSound(); // will output the cat sound!
+  j->makeSound();
+  meta->makeSound();
+  return 0;
 }
