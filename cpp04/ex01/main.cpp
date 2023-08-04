6ab790be90fe7ac040fa0fcd51c5078fef831adb
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-08-04 15:15:51 +0100

    update main

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:32:30 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/08/04 14:57:25 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/08/04 15:14:55 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,9 @@
 
 int main()
 {
-  // const Animal *j = new Dog();
-  // const Animal *i = new Cat();
-  // delete j; // should not create a leak
-  // delete i;
-  Cat a;
-  Cat b(a);
-
-  system("leaks ex01");
-
+  const Animal *j = new Dog();
+  const Animal *i = new Cat();
+  delete j; // should not create a leak
+  delete i;
   return 0;
 }
