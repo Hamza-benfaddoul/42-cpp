8bdc242974fb1a4fa2baf81317d5e23e340c97c7
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-08-12 17:20:50 +0100

    add test from subject

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:32:30 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/08/04 15:14:55 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/08/12 17:20:12 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,25 @@
 
 int main()
 {
-  const Animal *j = new Dog();
-  const Animal *i = new Cat();
-  delete j; // should not create a leak
-  delete i;
-  return 0;
+  {
+    const Animal *j = new Dog();
+    const Animal *i = new Cat();
+    delete j; // should not create a leak
+    delete i;
+  }
+  std::cout << "----------------------------------------------" << std::endl;
+  {
+    Animal *array[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+      if (i % 2 == 0)
+        array[i] = new Dog();
+      else
+        array[i] = new Cat();
+    }
+    for (int i = 0; i < 10; i++)
+      delete array[i];
+  }
+ return 0;
 }
