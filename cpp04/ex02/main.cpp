4d5c706451cdea643835bf913e9a0cf98a2c73c6
Hamza Benfaddoul <hbenfadd@e2r8p1.1337.ma>
2023-08-13 11:13:23 +0100

    add more test to ex02

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:32:30 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/08/03 17:22:16 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/08/13 11:13:10 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,47 @@
 #include "Dog.hpp"
 # include "Brain.hpp"
 
+void run(){
+{
+    const AAnimal *j = new Dog();
+    const AAnimal *i = new Cat();
+    delete j; // should not create a leak
+    delete i;
+  }
+  std::cout << "----------------------------------------------" << std::endl;
+  {
+    AAnimal *array[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+      if (i % 2 == 0)
+        array[i] = new Dog();
+      else
+        array[i] = new Cat();
+    }
+    for (int i = 0; i < 10; i++)
+      delete array[i];
+  }
+  std::cout << "----------------------------------------------" << std::endl;
+  {
+    Dog dog1;
+    Dog dog2;
+
+    dog1 = dog2;
+  }
+  {
+    Cat cat1;
+    Cat cat2;
+
+    cat1 = cat2;
+  }
+
+}
+
 int main()
 {
-  const AAnimal *j = new Dog();
-  const AAnimal *i = new Cat();
-  i->makeSound(); // should output the cat sound!
-  j->makeSound(); // should output the dog sound!
-  delete j; // should not create a leak
-  delete i;
-
-  return 0;
+  run();
+  
+  // system("leaks ex02");
+ return 0;
 }
