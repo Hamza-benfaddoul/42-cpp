bf4c9bc9a77af3557f4441eca500115925f71bbe
Hamza Benfaddoul <hbenfadd@e1r4p12.1337.ma>
2023-11-28 12:06:11 +0100

    update

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:16:52 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/11/21 10:17:32 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/11/27 16:16:17 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,21 @@
 #include <iostream>
 #include <stack>
 
-template <typename T>
-class MutantStack : public std::stack<T>
+template <typename T, typename myContainer=std::deque<T> >
+class MutantStack : public std::stack<T, myContainer>
 {
 	public:
-		MutantStack() : std::stack<T>(){}
-		MutantStack(MutantStack const &other) : std::stack<T>(other){}
+		MutantStack() : std::stack<T, myContainer>(){}
+		MutantStack(MutantStack const &other) : std::stack<T, myContainer>(other){}
 		MutantStack &operator=(MutantStack const &other)
 		{
 			if (this != &other)
-				std::stack<T>::operator=(other);
+				std::stack<T, myContainer>::operator=(other);
 			return (*this);
 		}
 		~MutantStack(){}
 
-		typedef typename std::stack<T>::container_type::iterator iterator;
-		iterator begin() { return (std::stack<T>::c.begin()); }
-		iterator end() { return (std::stack<T>::c.end()); }
+		typedef typename myContainer::iterator iterator;
+		iterator begin() { return (std::stack<T, myContainer>::c.begin()); }
+		iterator end() { return (std::stack<T, myContainer>::c.end()); }
 };
