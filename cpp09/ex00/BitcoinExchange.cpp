bada337ac60139f2c64619b75f18df856b267f5b
Hamza Benfaddoul <hbenfadd@e1r4p12.1337.ma>
2023-12-30 18:22:47 +0100

    update

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -62,7 +62,7 @@ double BitcoinExchange::convert(std::string const &date, double amount)
 	if (it == this->_dataBase.end())
 	{
 		_dataBase.insert(std::pair<std::string, double>(date, 0));
-		it = this->_dataBase.find(date);
+	it = this->_dataBase.find(date);
 		--it;
 		_dataBase.erase(date);
 		return (amount * it->second);
@@ -88,16 +88,21 @@ void	checkDate(std::string &date)
 	std::string day;
 	std::getline(iss, year, '-');
 	std::getline(iss, month, '-');
-	std::getline(iss, day, ' ');
-
+	std::getline(iss, day);
 	if (isDigitOnly(year) == false || isDigitOnly(month) == false || isDigitOnly(day) == false)
 		throw std::runtime_error("Error: bad input => " + iss.str());
-	if(std::strtod(year.c_str(), NULL) < 0 || std::strtod(year.c_str(), NULL) > 9999)
+	if(std::strtod(year.c_str(), NULL) <= 2008 || std::strtod(year.c_str(), NULL) > 9999)
 		throw std::runtime_error("Error: bad input => " + iss.str());
 	if(std::strtod(month.c_str(), NULL) < 0 || std::strtod(month.c_str(), NULL) > 12)
 		throw std::runtime_error("Error: bad input => " + iss.str());
 	if(std::strtod(day.c_str(), NULL) < 0 || std::strtod(day.c_str(), NULL) > 31)
 		throw std::runtime_error("Error: bad input => " +  iss.str());
+	if(std::strtod(month.c_str(), NULL) == 4 || std::strtod(month.c_str(), NULL) == 6
+		|| std::strtod(month.c_str(), NULL) == 9 || std::strtod(month.c_str(), NULL) == 11)
+	{
+		if(std::strtod(day.c_str(), NULL) > 30)
+			throw std::runtime_error("Error: bad input => " + iss.str());
+	}
 	if (std::strtod(month.c_str(), NULL) == 2 && std::strtod(day.c_str(), NULL) > 28 && (int)std::strtod(year.c_str(), NULL) % 4 != 0)
 		throw std::runtime_error("Error: bad input => " + iss.str());
 }
@@ -110,9 +115,9 @@ void	checkValue(std::string const &value)
 	if (value[0] == '-')
 		throw std::runtime_error("Error: not a positive number.");
 	val = std::strtod(value.c_str(), &end);
-	if (*end != '\0' || value.empty() || value[0] == ' ' || value[0] == '.')
+	if (*end != '\0' || value.empty() || std::isdigit(value[0]) == false)
 		throw std::runtime_error("Error: bad input" + value);
- 	if(val < 0 || val > 99999)
+ 	if(val < 0 || val > 10000)
 		throw std::runtime_error("Error: to large a number");
 }
 
@@ -129,20 +134,28 @@ void BitcoinExchange::read_file(std::string const &file)
 		getline(ifs, line);
 		if (line != "date | value")
 			throw std::runtime_error("Error: invalid file format");
-		while (std::getline(ifs, line))
+		while (std::getline(ifs, line, '\n'))
 		{
 			try
 			{
+				if (line.empty())
+					throw std::runtime_error("Error: empty line");
 				std::istringstream iss(line);
 				std::getline(iss, date, '|');
+				if (date.empty())
+					throw std::runtime_error("Error: bad input => " + iss.str());
 				if (date.at(date.length() - 1) == ' ')
 					date.erase(date.end() - 1, date.end()); // remove last char (space)
 				checkDate(date);
 				std::getline(iss, value);
+				if (value.empty())
+					throw std::runtime_error("Error: bad input => " + iss.str());
 				if(value.at(0) == ' ')
 					value.erase(value.begin(), value.begin() + 1); // remove first char (space)
 				checkValue(value);
 				std::cout << date << " => " << value << " = " << this->convert(date, std::strtod(value.c_str(), NULL)) << std::endl;
+				date.clear();
+				value.clear();
 			}
 			catch(const std::exception& e)
 			{
--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:22:24 by hbenfadd          #+#    #+#             */
-/*   Updated: 2023/12/17 15:49:22 by hbenfadd         ###   ########.fr       */
+/*   Updated: 2023/12/30 18:14:47 by hbenfadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int ac, char **av)
 {
 	(void)av;
-	if (ac != 2)
+	if (!ac)
 	{
 		std::cout << "Usage: ./bitcoin [inputfile]" << std::endl;
 		return (1);
@@ -23,7 +23,7 @@ int	main(int ac, char **av)
 	try
 	{
 		BitcoinExchange	btc("data.csv");
-		btc.read_file(av[1]);
+		btc.read_file("input.txt");
 	}
 	catch(const std::exception& e)
 	{
